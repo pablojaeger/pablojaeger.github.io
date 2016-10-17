166de905820fded065db0174e363129a072e13e4
Pablo Junior Jaeger <pablojuniorj@gmail.com>
2016-10-17 17:06:04 -0200

    teste umbler

--- tema/js/tema.js
+++ tema/js/tema.js
@@ -1,84 +1,52 @@
-
-
-
-function defineAlturaDestaque()
-{
-	var alturaJanela = $(window).height();
-	var alturaBarraNav = $("#nav_geral").outerHeight();
-	$("#capa").height(alturaJanela - alturaBarraNav);
-
-	$(".secao").each(function(){
-		$(this).css({"min-height":alturaJanela});
-	});
-}
- //**//**//**//**//**//**//**//**//**//**//**//
-function fixaMenu()
-{
-	var scrollPagina = $(window).scrollTop();
-	var valorScrollTopMenu = $("#nav_geral").offset().top;
-	var alturaBarraNav = $("#nav_geral").outerHeight();
-	if((valorScrollTopMenu - scrollPagina - alturaBarraNav)< 0)
-	{
-		if(!$("#nav_geral").hasClass("menu-fixo"))
-		{
-			$("#nav_geral").addClass("menu-fixo");
-		}
-	} 
-}
-
-
-// function identificaSecao(){
-// 	$(".link-secoes a").each(function() {
-
-// 		$(this).click(function(){
-// 		    $(".link-secoes").parent().removeClass('active');
-// 			$(this).parent().addClass('active');
-// 		});
-
-// 		/*
-// 		$(this).click(funtion(){
-// 			$(".link-secoes").removeClass('ativo');
-// 			$(this).addClass('ativo');
-// 		});
-// 		*/
-// 	});
-// }
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
-
-var lastScrollTop = 0;
-$(window).scroll(function(event)
-{
-	var st = $(this).scrollTop();
-	if (st > lastScrollTop)
-	{//desceu
-	   fixaMenu();
-	} 
-	else 
-	{//subiu
-	  $("#nav_geral").removeClass("menu-fixo");
+// Carousel tweets
+/*
+setInterval(function(){ 
+	if($(".tweets-carousel").css("left") >= 0){
+		$(this).css({ "left": "-100%" });
+		console.log("left>0");
 	}
-	lastScrollTop = st;
-});
+	else{
+		$(this).css({ "left": "0" });
+		console.log("left=0");
+	};
+	console.log($(".tweets-carousel").css("left"));
+}, 1000);
+*/
+ 
+//	...
+//	Menu mobile
+	
+	$(".show-menu").click(function(){
+		$(".mobile-menu").toggleClass("open");
+		$(".content-wraper").toggleClass("mobile-menu-open");
+	});
 
-$(window).scroll(function(){
-	fixaMenu();
-});
+//	...
+//	Anima  conforme a rolagem
+	function boxTop(idBox) {
+	  var boxOffset = $(idBox).offset().top;
+	  return boxOffset;
+	}
 
-$(window).resize(function()
-{
-	defineAlturaDestaque();
-});
+	$(document).ready(function() {
+	  var $target = $('.scroll-animation'),
+	      animationClass = 'pre-animation',
+	      windowHeight = $(window).height(),
+	      offset = windowHeight - (windowHeight / 4);
 
-defineAlturaDestaque();  
-fixaMenu();
-//identificaSecao();
+	  function animeScroll() {
+	    var documentTop = $(document).scrollTop();
+	    $target.each(function() {
+	      if (documentTop > boxTop(this) - offset) {
+	        $(this).removeClass(animationClass);
+	      } else {
+	        $(this).addClass(animationClass);
+	      }
+	    });
+	  }
+	  animeScroll();
+
+	  $(document).scroll(function() {
+	    animeScroll();
+	  });
+	});
